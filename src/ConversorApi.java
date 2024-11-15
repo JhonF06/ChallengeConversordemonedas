b1cfadd0d2ef6239600c5e5cbb0738740426db64
JhonF06 <Jfmorales195@gmail.com>
2024-11-15 01:42:12 -0500

    Add files via upload

---
+++ src/ConversorApi.java
@@ -0,0 +1,32 @@
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConversorApi {
+
+
+    public MonedasApi tasaDeCambio(String origen, String destino, double valor) {
+        String apiKey = "d60b25017b75a99cc6c03c52";
+        String direccion = "https://v6.exchangerate-api.com/v6/" + apiKey + "/pair/"+ origen + "/"
+                + destino + "/" + valor;
+        HttpResponse<String> response;
+        try {
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(direccion))
+                    .build();
+
+            response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), MonedasApi.class);
+
+        } catch (Exception e) {
+            throw new RuntimeException("No tengo contizacion existente a USD");
+        }
+
+
+    }
+}
---
+++ src/PrincipalConversor.java
@@ -0,0 +1,99 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class PrincipalConversor {
+    public static void main(String[] args) {
+        Scanner lectura = new Scanner(System.in);
+        ConversorApi consulta = new ConversorApi();
+        String origen = "";
+        String destino = "";
+        double valor;
+        int usuario = 0;
+
+        while (usuario != 9) {
+            System.out.println("""
+                Te damos bienvenida al conversor de moneda
+                ***********************************************
+                Seleecione la moneda que desea cambiar
+                1- Dólar =>> Peso argentino
+                2- Peso argentino =>> Dólar
+                3- Dólar =>> Real brasileñ0
+                4- Real brasileño =>> Dólar
+                5- Dólar =>> Peso colombiano
+                6- Peso colombiano =>> Dólar
+                7- Dólar =>> Peso chileno
+                8- Peso Chileno =>> Dólar
+                9- Salir.
+                """);
+            usuario = lectura.nextInt();
+
+            switch (usuario){
+                case 1:
+                    origen = "USD";
+                    destino = "ARS";
+                    break;
+                case 2:
+                    origen = "ARS";
+                    destino = "USD";
+                    break;
+                case 3:
+                    origen = "USD";
+                    destino = "BRL";
+                    break;
+                case 4:
+                    origen = "BRL";
+                    destino = "USD";
+                    break;
+                case 5:
+                    origen = "USD";
+                    destino = "COP";
+                    break;
+                case 6:
+                    origen = "COP";
+                    destino = "USD";
+                    break;
+                case 7:
+                    origen = "USD";
+                    destino = "CLP";
+                    break;
+                case 8:
+                    origen = "CLP";
+                    destino = "USD";
+                    break;
+                case 9:
+                    System.out.println("Gracias por usar el conversor, !Hasta Pronto!");
+                    break;
+
+                default:
+                    System.out.println("El numero ingresado no es valido, por favor intente nuevamente");
+                    break;
+            }
+            if (usuario >= 1 && usuario <= 8) {
+                System.out.println("Ingrese el valor que desea convertir");
+                valor = lectura.nextDouble();
+
+                if (valor > 0) {
+                    MonedasApi operacion = consulta.tasaDeCambio(origen, destino,valor);
+                    System.out.println("El valor de " + valor + " [" + origen +
+                            "] equivale a " + operacion.conversion_result() +
+                            " [" + destino + "]");
+
+                    GenerarJson generador = new GenerarJson();
+                    try {
+                        generador.archivo(operacion, origen, destino);
+                    } catch (IOException e) {
+                        throw new RuntimeException("Error al grabar datos Json");
+                    }
+                }else {
+                    System.out.println("Por favor ingrese un valor mayor a 0");
+                }
+
+            }
+
+
+
+        }
+
+        System.out.println("Fin del programa");
+    }
+}
